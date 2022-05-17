2de26ce2023fe20a3533c79737aee5466e4ec51f
mighty74 <103374849+mighty74@users.noreply.github.com>
2022-05-17 22:57:13 +0900

    Update settings.py

--- membermanage/settings.py
+++ membermanage/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m5_#-56ow!3%&3k98#8#rqmbgg3(+z7r5ybli*xni*o2@wr30e'
+SECRET_KEY = '********'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
